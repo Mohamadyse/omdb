6d24983a5e49de8f72fa1bcaf980f31c00a006a9
Mohamad Eddin <mohamad.eddin@skandia.se>
2019-02-23 02:28:03 +0100

    built GetMovie() in Repository class

--- Core/Repositories/MovieRepository.cs
+++ Core/Repositories/MovieRepository.cs
@@ -5,12 +5,13 @@ using Core.Interfaces;
 using Data.Models;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Core.Repositories
 {
     public class MovieRepository : IMovieRepository
     {
-        private readonly HttpClient _httpClient;
+       // private readonly HttpClient _httpClient;
         private ILogger<IMovieRepository> _logger;
         private const string ApiKey = "f8940a38";
         private const string BaseUrl = "http://www.omdbapi.com/";
@@ -18,27 +19,46 @@ namespace Core.Repositories
         public MovieRepository(ILogger<IMovieRepository> logger)
         {
             _logger = logger;
-            _httpClient = new HttpClient();
+           
         }
 
         public async Task<Movie> GetMovie(string queryParameters)
         {
-            try
-            {
-                var url = $"{BaseUrl}?apikey={ApiKey}{queryParameters}";
-                var responseMessage = await _httpClient.GetAsync(url);
+            var url = $"{BaseUrl}?apikey={ApiKey}{queryParameters}";
 
-                var content = await responseMessage.Content.ReadAsStringAsync();
 
-                // Remove this and instead map JSON from the response to a list of the actual model "Movie"
-                // Use JsonConvert to deserialize from Json to Movie
-                return new Movie();
-            }
-            catch (Exception ex)
-            {
-                // Log and handle the exception
-                throw; 
-            }
+            // för att HttpCient är disposal ät det bättre att använda using istället
+            using (HttpClient _httpClient = new HttpClient())
+                try
+                {
+                    
+                    using (var responseMessage = await _httpClient.GetAsync(url))
+                    {
+
+                        var content = await responseMessage.Content.ReadAsStringAsync();
+
+                        JObject json = JObject.Parse(content);
+                        return json.ToObject<Movie>();
+
+
+
+
+                        // Remove this and instead map JSON from the response to a list of the actual model "Movie"
+                        // Use JsonConvert to deserialize from Json to Movie
+                      //  dynamic jsonMovie = JsonConvert.DeserializeObject(content);
+
+
+
+
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log and handle the exception
+                    _logger.LogDebug(ex.Message);
+                   
+                }
         }
     }
 }
---
+++ Core/mappers/JsonToMovie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Data.Models;
+using Newtonsoft.Json.Linq;
+
+namespace Core.mappers
+{
+    public class JsonToMovie
+    {
+        private readonly JObject _json;
+
+        public JsonToMovie  (JObject json)
+        {
+            _json = json;
+        }
+
+        public static Movie Convert(JObject json)
+        {
+           
+
+            return  new Movie();
+            
+        }
+     }
+}
